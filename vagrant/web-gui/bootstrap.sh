544d46d5e6052b69796cacaf30307eacc2d7b164
Joaquin Casares <joaquin@datastax.com>
2014-10-31 11:15:53 -0500

    Remove dockerfiles from web-gui repo

--- vagrant/web-gui/bootstrap.sh
+++ vagrant/web-gui/bootstrap.sh
@@ -21,21 +21,23 @@ chmod 600 .ssh/*
 
 # needed to simulate a local dev environment
 sudo chown -R ubuntu:ubuntu webapps
-mkdir webapps/datastax-dockerfiles
-git clone git@datastax-dockerfiles:datastax-demos/datastax-dockerfiles.git webapps/datastax-dockerfiles
 
 # setup vagrant to use the vagrant-aws and vagrant-awsinfo plugins
 vagrant box add aws-dummy webapps/demos-web-gui/vagrant/vagrant-boxes/aws-dummy.box
 vagrant plugin install vagrant-aws vagrant-awsinfo
 
 # ensure we have a copy of the Vagrantfile
-cd ~/webapps/demos-web-gui/
-if [ ! -f vagrant/single-node-demo/Vagrantfile ]; then
-    git checkout vagrant/single-node-demo/Vagrantfile
-fi
-if [ ! -f vagrant/web-gui/Vagrantfile ]; then
-    git checkout vagrant/web-gui/Vagrantfile
-fi
+(
+    cd ~/webapps/demos-web-gui/
+    if [ ! -f vagrant/single-node-demo/Vagrantfile ]; then
+        git checkout vagrant/single-node-demo/Vagrantfile
+    fi
+    if [ ! -f vagrant/web-gui/Vagrantfile ]; then
+        git checkout vagrant/web-gui/Vagrantfile
+    fi
+)
+
+mkdir launched-clusters
 
 # ensure credentials are set on each launch
 echo "source webapps/demos-web-gui/set_credentials.sh" >> .profile
