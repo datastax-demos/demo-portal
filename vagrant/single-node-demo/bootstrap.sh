3b3d548e1128ebb7566a0ff64ec989f3bfabacf7
Joaquin Casares <joaquin@datastax.com>
2014-10-31 14:08:32 -0500

    pull images before using

--- vagrant/single-node-demo/bootstrap.sh
+++ vagrant/single-node-demo/bootstrap.sh
@@ -6,9 +6,10 @@ sudo apt-get install -y build-essential tree git-core
 
 # setup the ~/ssh keys. ssh-keyscan github.com before cloning. chmod 600 ~/.ssh.
 cp webapps/demos-web-gui/vagrant/keys/* .ssh
-mv .ssh/.dockercfg .
+cp webapps/demos-web-gui/vagrant/keys/.dockercfg .
 ssh-keyscan github.com >> .ssh/known_hosts
 chmod 600 .ssh/*
+chmod 600 .dockercfg
 
 # ensure credentials are set on each launch
 echo "source webapps/demos-web-gui/set_credentials.sh" >> .profile
