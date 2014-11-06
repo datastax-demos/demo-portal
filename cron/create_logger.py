0257cd309a7ff9c3420ce6ecfeeb6714dc15193b
Joaquin Casares <joaquin@datastax.com>
2014-11-06 14:54:49 -0600

    ttl cronjob

---
+++ cron/create_logger.py
@@ -0,0 +1,16 @@
+import logging
+
+LOGGER_NAME = 'cleanup'
+
+logger = logging.getLogger(LOGGER_NAME)
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('%s.log' % LOGGER_NAME)
+fh.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    '[%(asctime)s] %(levelname)s %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+logger.addHandler(fh)
+logger.addHandler(ch)
