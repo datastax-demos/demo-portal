3bafdc16a9100410bc7902d9e706845e8ae24990
Joaquin Casares <joaquin@datastax.com>
2014-11-06 15:45:21 -0600

    change cron list name

--- vagrant/web-gui/bootstrap.sh
+++ vagrant/web-gui/bootstrap.sh
@@ -54,4 +54,4 @@ nohup python ~/webapps/demos-web-gui/flask/web-gui.py > out.log 2>&1 &
 echo "source ~/webapps/demos-web-gui/set_credentials.sh" >> .profile
 
 # setup ec2 cleaning cron job
-sudo cp ~/webapps/demos-web-gui/cron/demos-web-gui.list /etc/cron.d/
+sudo cp ~/webapps/demos-web-gui/cron/demos-web-gui.list /etc/cron.d/demoswebguicron
