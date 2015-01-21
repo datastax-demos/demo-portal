5fa58b10edf88d72844aeec991299436aa3aa3e1
Joaquin Casares <joaquin@datastax.com>
2015-01-21 16:53:09 -0600

    Verified dev launches

--- vagrant/demo-portal/src/run.sh
+++ vagrant/demo-portal/src/run.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-if [ -n "$PRODUCTION" ]; then
-    # kick off the demo-portal
-    nohup python /portal/demo-portal/flask2.0/run > out.log 2>&1 &
-fi
+# kick off the demo-portal
+nohup python /portal/demo-portal/flask2.0/run > out.log 2>&1 &
